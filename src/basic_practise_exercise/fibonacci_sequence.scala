62f7a574983e314ece4c85d82202aba5ea9963c8
Odeussea <lorrainemei@gmail.com>
2012-12-31 12:08:44 +0100

    Calculate the nth number of fibonacci sequence by recursion

--- src/basic_practise_exercise/fibonacci_sequence.scala
+++ src/basic_practise_exercise/fibonacci_sequence.scala
@@ -2,4 +2,10 @@ package basic_practise_exercise
 
 object fibonacci_sequence extends App {
 
+// Generate the nth number of fibonacci number by recursion WoW
+
+  def fib_gen (n:Int) : Int = n match {
+    case 0 | 1 => n
+    case _ => fib_gen(n-1) + fib_gen(n-2)
+  }
 }
